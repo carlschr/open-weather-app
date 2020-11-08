969d278c871e0cd2b73623ae6807e6cf0dd60e3b
carlschr <cjcarlson136@gmail.com>
2020-11-08 01:21:54 -0600

    Logic for adding and styling uv index

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const renderCurrentWeather = location => {
         method: 'GET'
     };
 
-    $.ajax(settings).done(response => {
+    $.ajax(settings).done(async response => {
         //Variables created using JSON data
         let cityName = response.name;
         let currentTemp = response.main.temp;
@@ -40,17 +40,34 @@ const renderCurrentWeather = location => {
         let cloudCover = response.weather[0].main;
         let weatherIcon = response.weather[0].icon;
         let currentWind = response.wind.speed;
+        //Grabs uv index from another API call using await keyword
+        let uvJSON = await $.ajax({url: `http://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=59c9f9d794fad5805a3ff6749acde141`, method: 'GET'});
+        console.log(uvJSON);
+        let uvIndex = uvJSON.value;
 
         //Header with city's name
-        let $currentHeader = $(`<h1 class="current-header">${cityName}:</h1>`);
+        let today = new Date(Date.now());
+        let $currentHeader = $(`<h1 class="current-header">${cityName} (${today.getMonth() + 1}/${today.getDate() + 1}/${today.getFullYear()})</h1>`);
         $('.current').prepend($currentHeader);
 
         //Weather info
-        let $currentWeather = $(`<p>Cloud Cover: ${cloudCover}</p><p>Current Temp: ${currentTemp} &#176;F</p><p>Humidity: ${currentHumidity} %</p><p>Wind Speed: ${currentWind} mph</p>`);
+        let $currentWeather = $(`<p>Cloud Cover: ${cloudCover}</p><p>Current Temp: ${currentTemp} &#176;F</p><p>Humidity: ${currentHumidity} %</p><p>Wind Speed: ${currentWind} mph</p><button class='uv'>UV: ${uvIndex}</button>`);
         $('.content').prepend($currentWeather);
 
         //Cloud cover icon
-        $('.icon').html(`<img src='http://openweathermap.org/img/wn/${weatherIcon}.png'/>`)
+        $('.icon').html(`<img src='http://openweathermap.org/img/wn/${weatherIcon}.png'/>`);
+
+        //Returns a color based on the uv index
+        const uvCheck = index => {
+            let color = (index < 3) ? 'green' : 
+            (index < 6) ? 'yellow' :
+            (index < 8) ? 'orange' :
+            (index < 11) ? 'red' : 'purple';
+            return color;
+        };
+
+        //Sets appropriate bg color for uv button;
+        $('.uv').attr('style', `background-color: ${uvCheck(uvIndex)};`);
 
         //Calls renderFiveDay with latitude and longitude
         renderFiveDay(response.coord.lat, response.coord.lon);
